6668abbea4f7ac6445cfbc85833e7de8b31fbb44
Chriss <devcshort@outlook.com>
2020-07-16 19:42:45 -0700

    Prepare for v1 release

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const webpack = require('webpack');
 
 const config = require('./webpack.config');
 
@@ -7,13 +6,6 @@ module.exports = merge(config, {
     mode: 'production',
     entry: './src/components/react-hls-player.js',
     devtool: 'source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        })
-    ],
     externals: {
         'react': 'commonjs react'
     }
